35d3345036b7206b520ede185f5a28049744aa16
Shankar <mshanka1@gmail.com>
2024-07-11 13:45:17 -0700

    to test

--- pythonProject/CV_Display.py
+++ pythonProject/CV_Display.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('about me')
+col1, col2 = st.columns(2,gap='small', vertical_alignment="center")
+with col1:
+    st.image("./CV/Photo.jpg", width=230)
+with col2:
+    st.title("Shankar Manne", anchor=False)
+    st.write(
+        "Senior Embedded Software Engineer with 19 Yrs of exp in developing both "
+        "Low level and High Level applications"
+    )
