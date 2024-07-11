0c103c185f3fbb526a4e73df0cf4a8c5bc011602
Shankar <mshanka1@gmail.com>
2024-07-11 14:04:00 -0700

    to test

--- pythonProject/CV_Display.py
+++ pythonProject/CV_Display.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 col1, col2 = st.columns(2,gap='small', vertical_alignment="center")
 with col1:
-    st.image("/CV/Photo.png", width=230)
+    st.image("./CV/Photo.png", width=230)
 with col2:
     st.title("Shankar Manne", anchor=False)
     st.write(
