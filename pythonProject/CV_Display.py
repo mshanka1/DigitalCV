e9bb40cd14d485df187507d535d125303077d79c
Shankar <mshanka1@gmail.com>
2024-07-11 14:37:42 -0700

    to test

--- pythonProject/CV_Display.py
+++ pythonProject/CV_Display.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
+
 image_str = "../CV/Photo.png"
-col1, col2 = st.columns(2,gap='small', vertical_alignment="center")
-with col1:
-    st.image(image_str, width=230)
-with col2:
-    st.title("Shankar Manne", anchor=False)
-    st.write(
-        "Senior Embedded Software Engineer with 19 Yrs of exp in developing both "
-        "Low level and High Level applications."
-    )
+st.title(image_str)
+#col1, col2 = st.columns(2,gap='small', vertical_alignment="center")
+#with col1:
+#    st.image(image_str, width=230)
+#with col2:
+#    st.title("Shankar Manne", anchor=False)
+#    st.write(
+#        "Senior Embedded Software Engineer with 19 Yrs of exp in developing both "
+#        "Low level and High Level applications."
+#    )
