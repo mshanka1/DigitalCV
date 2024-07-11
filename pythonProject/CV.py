e08a761f496544b692e07b85d9025a5c969f9ccd
Shankar <mshanka1@gmail.com>
2024-07-11 13:37:58 -0700

    to test

--- pythonProject/CV.py
+++ pythonProject/CV.py
@@ -2,13 +2,17 @@ import streamlit as st
 
 #--- Page Setup---
 about_page = st.Page(
-    page="CVHTML/cv.html",
+    page="CV_Display.py",
     title="About Me",
     default=True,
 )
 
 #--- Navigation Page---
-pg= st.navigation(pages=[about_page])
+pg= st.navigation(
+    {
+        "Info": [about_page],
+    }
+)
 
 #--- Run Navigation ---
 pg.run()
