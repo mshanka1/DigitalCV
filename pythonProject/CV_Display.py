8876e7e21f5862fe0f019a349a0e0e1dcc1afb57
Shankar <mshanka1@gmail.com>
2024-07-11 14:45:09 -0700

    to test

--- pythonProject/CV_Display.py
+++ pythonProject/CV_Display.py
@@ -2,16 +2,13 @@ import streamlit as st
 import os
 
 fileDir = os.path.dirname(os.path.realpath('__file__'))
-filename = os.path.join(fileDir, 'pythonProject/CV/Photo.png')
-#image_str = "../CV/Photo.png"
-#st.title(filename)
-st.image(filename, width=230)
-#col1, col2 = st.columns(2,gap='small', vertical_alignment="center")
-#with col1:
-#    st.image(image_str, width=230)
-#with col2:
-#    st.title("Shankar Manne", anchor=False)
-#    st.write(
-#        "Senior Embedded Software Engineer with 19 Yrs of exp in developing both "
-#        "Low level and High Level applications."
-#    )
+image_str = os.path.join(fileDir, 'pythonProject/CV/Photo.png')
+col1, col2 = st.columns(2,gap='small', vertical_alignment="center")
+with col1:
+    st.image(image_str, width=230)
+with col2:
+    st.title("Shankar Manne", anchor=False)
+    st.write(
+        "Senior Embedded Software Engineer with 19 Yrs of exp in developing both "
+        "Low level and High Level applications."
+    )
